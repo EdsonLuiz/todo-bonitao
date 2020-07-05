cfb4c0c74b92a3c5328c0b27605374a4fa5bc85a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-07-04 21:09:08 -0300

    Add style

--- script.js
+++ script.js
@@ -25,14 +25,20 @@ function renderTodoList() {
   const listItems = todos.map((todo, index) => (
     `
     <li>
-      <button type="button" onclick="changeState(${index})">Change</button>
-      ${todo.title}
-      <button type="button" onclick="deleteTodo(${index})">Deletar</button>
+      <button class="btn btn-checked" type="button" onclick="changeState(${index})">
+        <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path class=${todo.isCompleted ? "svg-check" : "svg-uncheck"} fill-rule="evenodd" clip-rule="evenodd" d="M6 10L4 12L10 18L20 8L18 6L10 14L6 10Z" fill="#29CB00"/>
+        </svg>
+      </button>
+      <span>${todo.title}</span>
+      <button class="btn btn-delete" type="button" onclick="deleteTodo(${index})">
+        <img src="./assets/delete.svg" alt="delete">
+      </button>
     </li>
     `
   ))
 
-  listElement.innerHTML = listItems
+  listElement.innerHTML = listItems.join("")
   return
 }
 
@@ -46,6 +52,7 @@ function handleSubmit() {
 
 function changeState(pos) {
   todos[pos].isCompleted = !todos[pos].isCompleted
+  renderTodoList()
   return
 }
 
@@ -56,4 +63,4 @@ function deleteTodo(pos) {
   return
 }
 
-renderTodoList(todos)
+renderTodoList()
